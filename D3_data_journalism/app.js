b3a3b2ca715ccfbc36718c1e1b3473cad3770085
jacobstryker93 <jacobstryker93@gmail.com>
2021-01-20 22:36:01 -0700

    Beginning function and svg parameters

---
+++ D3_data_journalism/app.js
@@ -0,0 +1,13 @@
+// @TODO: YOUR CODE HERE!
+function interactiveGraphs() {
+
+    let svgWidth = 800;
+    let svgHeight = 400;
+    
+    let margin = {
+    top: 10,
+    right: 20,
+    bottom: 40,
+    left:50
+    };
+    
